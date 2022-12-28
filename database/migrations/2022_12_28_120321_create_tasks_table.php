daf022dc6c138e9b656a38e238b3151582ffe415
Lis Shporta <lis.shporta@gmail.com>
2022-12-28 13:19:01 +0100

    Project Update

--- database/migrations/2022_12_28_120321_create_tasks_table.php
+++ database/migrations/2022_12_28_120321_create_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('description');
-            $table->string('status');
+            $table->string('status')->nullable();
             $table->timestamps();
         });
     }
