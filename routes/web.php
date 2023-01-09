a47dd803706050a64bff5b9d84ba28b6ef4a3fd9
Lis Shporta <lis.shporta@gmail.com>
2023-01-09 22:25:25 +0100

    Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 
 // Show main page 
 Route::get('/',[TaskController::class, 'index']);
@@ -11,3 +24,15 @@ Route::get('/tasks/create',[TaskController::class,'create']);
 
 // Create Task
 Route::post('/tasks/store',[TaskController::class,'store']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
