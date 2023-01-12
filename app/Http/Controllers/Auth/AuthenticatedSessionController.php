ebb8750f081ff39fdd5f94dd41111d08179dc4f4
Lis Shporta <lis.shporta@gmail.com>
2023-01-12 16:18:40 +0100

    Auth Flash Message

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return redirect()->intended(RouteServiceProvider::HOME)->with(['success' => 'Logged In !']);
     }
 
     /**
@@ -43,6 +43,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/')->with(['success' => 'Logged Out!']);
     }
 }
